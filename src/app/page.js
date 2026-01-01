6fa3318612386ee03088e35089960da4ef75163d
Prashant Bisht <prashantbisht284@gmail.com>
2026-01-01 22:03:06 +0530

    .

---
+++ src/app/page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button } from '@/components/ui/button'
+
+const page = () => {
+  return (
+    <div className='flex justify-center items-center h-screen'>
+    <Button>click</Button>
+    </div>
+  )
+}
+
+export default page
