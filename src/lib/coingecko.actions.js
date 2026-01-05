a011ca37b8a3d3bb050da00cc89a6c37613ec8e6
Prashant Bisht <prashantbisht284@gmail.com>
2026-01-05 14:53:42 +0530

    api call and error handling

---
+++ src/lib/coingecko.actions.js
@@ -0,0 +1,39 @@
+'use server';
+import qs from 'query-string';
+
+
+const BASE_URL = process.env.COINGECKO_BASE_URL;
+const API_KEY = process.env.COINGECKO_API_KEY;
+
+if(!BASE_URL) throw new Error('Could not get base url');
+if(!API_KEY) throw new Error('Could not get API key');
+
+export async function fetcher(
+  endpoint,
+  params,
+  revalidate = 60
+) {
+  const url = qs.stringifyUrl({
+    url: `${BASE_URL}/${endpoint}`,
+    query: params,
+  },{skipEmptyString: true, skipNull: true});
+
+  const response = await fetch(url,
+    {
+        headers:{
+            "x-ch-pro-api-key":API_KEY,
+            "Content-Type":"application/json",
+        },
+        next: { revalidate}
+    }
+  );
+
+  if(!response.ok){
+    const errorBody  = await response.json()
+    .catch(()=>({
+    }));
+
+    throw new Error(`API Error: ${response.status}: ${errorBody.error || response.statusText}`);
+  }
+  return response.json();
+}
